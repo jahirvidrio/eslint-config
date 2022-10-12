8021302ca7797aab6d0aeffab968c9573f527fa3
Jahir Vidrio <me@jahirvidrio.dev>
2022-10-12 13:07:20 -0500

    fix: typescript rules not loaded

--- typescript.js
+++ typescript.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: './index.js',
+  extends: ['./index.js', './rules/typescript.js'],
   plugins: ['@typescript-eslint'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -24,5 +24,37 @@ module.exports = {
     // Resolve type definition packages
     'import/external-module-folders': ['node_modules', 'node_modules/@types'],
   },
-  rules: {},
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        // The following rules are enabled in Airbnb config, but are already checked (more thoroughly) by the TypeScript compiler
+        // Some of the rules also fail in TypeScript files, for example: https://github.com/typescript-eslint/typescript-eslint/issues/662#issuecomment-507081586
+        // Rules are inspired by: https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/configs/eslint-recommended.ts
+        'constructor-super': 'off',
+        'getter-return': 'off',
+        'no-const-assign': 'off',
+        'no-dupe-args': 'off',
+        'no-dupe-class-members': 'off',
+        'no-dupe-keys': 'off',
+        'no-func-assign': 'off',
+        'no-import-assign': 'off',
+        'no-new-symbol': 'off',
+        'no-obj-calls': 'off',
+        'no-redeclare': 'off',
+        'no-setter-return': 'off',
+        'no-this-before-super': 'off',
+        'no-undef': 'off',
+        'no-unreachable': 'off',
+        'no-unsafe-negation': 'off',
+        'valid-typeof': 'off',
+        // The following rules are enabled in Airbnb config, but are recommended to be disabled within TypeScript projects
+        // See: https://github.com/typescript-eslint/typescript-eslint/blob/13583e65f5973da2a7ae8384493c5e00014db51b/docs/linting/TROUBLESHOOTING.md#eslint-plugin-import
+        'import/named': 'off',
+        'import/no-named-as-default-member': 'off',
+        // Disable `import/no-unresolved`, see README.md for details
+        'import/no-unresolved': 'off',
+      },
+    },
+  ],
 };
